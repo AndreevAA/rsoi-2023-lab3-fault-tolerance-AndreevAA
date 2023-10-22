370d3fb5e2d5b9d65241cdcbe0f9335d726a1d67
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2023-10-22 19:50:22 +0300

    Added get data from Service queue

--- app/queue/queue_service/service_requests.py
+++ app/queue/queue_service/service_requests.py
@@ -3,3 +3,13 @@ from queue_service.circuit_breaker import CircuitBreaker
 
 circuit_breaker = CircuitBreaker()
 
+# Получения данных из очереди
+def get_data_from_service(service_url, headers={}, timeout=10):
+    if circuit_breaker.try_connect(service_url):
+        try:
+            response = requests.get(service_url, timeout=timeout, headers=headers)
+            circuit_breaker.connection_successful(service_url)
+            return response
+        except:
+            circuit_breaker.connection_error(service_url)
+            return None
