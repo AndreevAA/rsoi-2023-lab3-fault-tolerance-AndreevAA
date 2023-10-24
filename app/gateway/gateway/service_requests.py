ba027b7eb465855244063cf1f17c70ee304588c7
Andreev Alexander <andreev.business.mail@gmail.com>
2023-10-24 16:24:14 +0300

    updated queue operations

--- app/gateway/gateway/service_requests.py
+++ app/gateway/gateway/service_requests.py
@@ -3,23 +3,35 @@ from gateway.circuit_breaker import CircuitBreaker
 
 circuit_breaker = CircuitBreaker()
 
-def make_request(method, service_url, headers={}, timeout=10, data=None):
+
+def get_data_from_service(service_url, headers={}, timeout=10):
     if circuit_breaker.try_connect(service_url):
         try:
-            response = requests.request(method, service_url, timeout=timeout, headers=headers, json=data)
+            response = requests.get(service_url, timeout=timeout, headers=headers)
             circuit_breaker.connection_successful(service_url)
             return response
         except:
             circuit_breaker.connection_error(service_url)
             return None
 
-def get_data_from_service(service_url, headers={}, timeout=10):
-    return make_request("GET", service_url, headers=headers, timeout=timeout)
-
 
 def post_data_from_service(service_url, headers={}, timeout=10, data={}):
-    return make_request("POST", service_url, headers=headers, timeout=timeout, data=data)
+    if circuit_breaker.try_connect(service_url):
+        try:
+            response = requests.post(service_url, timeout=timeout, headers=headers, json=data)
+            circuit_breaker.connection_successful(service_url)
+            return response
+        except:
+            circuit_breaker.connection_error(service_url)
+            return None
 
 
 def delete_data_from_service(service_url, headers={}, timeout=10):
-    return make_request("DELETE", service_url, headers=headers, timeout=timeout)
+    if circuit_breaker.try_connect(service_url):
+        try:
+            response = requests.delete(service_url, timeout=timeout, headers=headers)
+            circuit_breaker.connection_successful(service_url)
+            return response
+        except:
+            circuit_breaker.connection_error(service_url)
+            return None
