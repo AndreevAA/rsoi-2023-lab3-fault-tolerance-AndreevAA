2d0764ee0121c1b830d95d4a3354e30c2a9e051f
Andreev Alexander <andreev.business.mail@gmail.com>
2023-10-24 16:19:20 +0300

    Req. service optimized for internal

--- app/gateway/gateway/service_requests.py
+++ app/gateway/gateway/service_requests.py
@@ -3,35 +3,23 @@ from gateway.circuit_breaker import CircuitBreaker
 
 circuit_breaker = CircuitBreaker()
 
-
-def get_data_from_service(service_url, headers={}, timeout=10):
+def make_request(method, service_url, headers={}, timeout=10, data=None):
     if circuit_breaker.try_connect(service_url):
         try:
-            response = requests.get(service_url, timeout=timeout, headers=headers)
+            response = requests.request(method, service_url, timeout=timeout, headers=headers, json=data)
             circuit_breaker.connection_successful(service_url)
             return response
         except:
             circuit_breaker.connection_error(service_url)
             return None
+
+def get_data_from_service(service_url, headers={}, timeout=10):
+    return make_request("GET", service_url, headers=headers, timeout=timeout)
 
 
 def post_data_from_service(service_url, headers={}, timeout=10, data={}):
-    if circuit_breaker.try_connect(service_url):
-        try:
-            response = requests.post(service_url, timeout=timeout, headers=headers, json=data)
-            circuit_breaker.connection_successful(service_url)
-            return response
-        except:
-            circuit_breaker.connection_error(service_url)
-            return None
+    return make_request("POST", service_url, headers=headers, timeout=timeout, data=data)
 
 
 def delete_data_from_service(service_url, headers={}, timeout=10):
-    if circuit_breaker.try_connect(service_url):
-        try:
-            response = requests.delete(service_url, timeout=timeout, headers=headers)
-            circuit_breaker.connection_successful(service_url)
-            return response
-        except:
-            circuit_breaker.connection_error(service_url)
-            return None
+    return make_request("DELETE", service_url, headers=headers, timeout=timeout)
