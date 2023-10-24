36b30885d24e90fd1d583015c62f5ebb739aff5b
Andreev Alexander <andreev.business.mail@gmail.com>
2023-10-24 16:14:39 +0300

    update from external to internal requests

--- app/gateway/gateway/circuit_breaker.py
+++ app/gateway/gateway/circuit_breaker.py
@@ -7,36 +7,30 @@ CIRCUIT_BREAKER_CONNECT_TRIES = 5
 
 
 class CircuitBreaker:
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(CircuitBreaker, cls).__new__(cls)
+    instance = None
+
+    @classmethod
+    def new(cls):
+        cls.instance = cls.instance or cls()
         return cls.instance
 
     def __init__(self):
         self.urls_data = {}
 
     def try_connect(self, url):
-        if url not in self.urls_data.keys():
-            self.urls_data[url] = [0, time.time()]
-            return True
-        else:
-            if self.urls_data[url][0] == -1:
-                if self.urls_data[url][1] < time.time():
-                    return False
-                else:
-                    if random.randint(0, 100) >= CIRCUIT_BREAKER_CONNECT_CHANCE:
-                        return False
+        data = self.urls_data.setdefault(url, [0, time.time()])
+        if data[0] == -1 and data[1] >= time.time():
+            return False
+        elif data[0] == -1 or random.randint(0, 100) >= CIRCUIT_BREAKER_CONNECT_CHANCE:
+            return False
         return True
 
     def connection_error(self, url):
-        if self.urls_data[url][0] == -1:
-            self.urls_data[url][1] = time.time() + CIRCUIT_BREAKER_BAN_TIME
-            return
-
-        self.urls_data[url][0] += 1
-        if self.urls_data[url][0] >= CIRCUIT_BREAKER_CONNECT_TRIES:
-            self.urls_data[url][0] = -1
-            self.urls_data[url][1] = time.time() + CIRCUIT_BREAKER_BAN_TIME
+        data = self.urls_data[url]
+        data[0] += 1
+        if data[0] >= CIRCUIT_BREAKER_CONNECT_TRIES:
+            data[0] = -1
+            data[1] = time.time() + CIRCUIT_BREAKER_BAN_TIME
 
     def connection_successful(self, url):
         self.urls_data[url][0] = 0
